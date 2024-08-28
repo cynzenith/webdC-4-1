66a389035700f8a60b1dc2be18470255a2fb808f
cynzenith~ <cynzenith@gmail.com>
2024-08-28 17:59:44 +0900

    menu

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
-$(function() {
+// $(function() {
 
-// menu
-    $(".nav > ul > li").mouseover(function(){
-        // alert("Click");
-        $(this).find(".submenu").stop().slideDown(500);
+// // menu
+//     $(".nav > ul > li").mouseover(function(){
+//         // alert("Click");
+//         $(this).find(".submenu").stop().slideDown(500);
+//     });
+
+//     $(".nav > ul > li").mouseout(function(){
+//         $(this).find(".submenu").stop().slideUp(100);
+//     });
+
+// // slider
+//     function moveSlider(){
+//         $(".sliderWrap").animate({marginLeft:"-800px"},1000,
+//             function(){
+//                 $(".sliderWrap").append($(".sliderWrap .slider").first());
+//                 $(".sliderWrap").css({marginLeft:0});
+//             });
+//     }
+//     setInterval(moveSlider, 3000);
+
+// // popup
+//     $(".popup").click(function(){
+//         $(".popup-cont").show();
+//     });
+//     $(".popup-close").click(function(){
+//         $(".popup-cont").hide();
+//     });
+
+
+
+// });  
+
+$(function(){
+    $(".nav ul>li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown(300);
     });
-
-    $(".nav > ul > li").mouseout(function(){
+    $(".nav ul>li").mouseout(function(){
         $(this).find(".submenu").stop().slideUp(100);
     });
-
-// slider
-    function moveSlider(){
-        $(".sliderWrap").animate({marginLeft:"-800px"},1000,
-            function(){
-                $(".sliderWrap").append($(".sliderWrap .slider").first());
-                $(".sliderWrap").css({marginLeft:0});
-            });
-    }
-    setInterval(moveSlider, 3000);
-
-// popup
-    $(".popup").click(function(){
-        $(".popup-cont").show();
-    });
-    $(".popup-close").click(function(){
-        $(".popup-cont").hide();
-    });
-
-
-
-});  
-
+});
